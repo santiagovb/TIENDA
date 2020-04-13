c959882a82629028089249b8d45248f51bdb71b5
Santiagovb <sdvargas@bancolombia.com.co>
2020-04-12 21:52:51 -0500

    carrito completo falta descontar productos de db :)

--- src/paginas/Carrito.js
+++ src/paginas/Carrito.js
@@ -3,7 +3,8 @@ import ListaCarrito from '../components/ListaCarrito'
 import BtnPagar from '../components/BtnPagar'
 import BtnIrATienda from '../components/BtnIrATienda'
 import estilos from '../components/estilos/carrito.css'
-// eslint-disable-next-line no-unused-vars
+import estilosfactura from '../components/estilos/factura.css'
+
 
 
 class Carrito extends Component {
@@ -19,20 +20,30 @@ class Carrito extends Component {
         let { database, carrito, totalitems, totalcompra } = this.state
         return (
 
-            <div className="contenedor">
-                <div className="fixed">
-                    <BtnIrATienda></BtnIrATienda>
+            <div className="contcarrito">
+
+                <div className="elemento1">
+                   {/*  <BtnIrATienda></BtnIrATienda> */}
                     <ListaCarrito
                         productos={database}
                         carrito={carrito}
                     ></ListaCarrito>
                 </div>
-                <div>
-                    <div className="flex-item">
-                        <div>Total items {totalitems}</div>
-                        <div>Valor total de tu compra $ {totalcompra.toLocaleString('es-CO')}</div>
-                        <BtnPagar></BtnPagar>
-                        <div>Seleccione el medio de pago</div>
+
+                <div className="elemento2">
+                    <div className="contentfactura">
+                        <div className="elementofactura fondogris">Total items: 
+                        <p>{totalitems}</p>
+                        </div>
+                        <div className="elementofactura fondomenosgris">Valor total de tu compra 
+                        <p>$ {totalcompra.toLocaleString('es-CO')}</p>
+                        </div>
+                        <p className="fondogris">Seleccione el medio de pago</p>
+                        <div className="elementofactura fondomenosgris ">
+                            <img className="imagen" src="https://upload.wikimedia.org/wikipedia/commons/7/72/MasterCard_early_1990s_logo.png" alt=""/>
+                           <img className="imagen" src="https://s3.amazonaws.com/nequi-wp-colombia/wp-content/app/wp-content/uploads/2019/08/QR-B.png" alt=""/>
+                            <img className="imagen" src="https://images.squarespace-cdn.com/content/v1/5a1899b251a584c59851ac92/1553621616656-ZHE8W4QE20ZM4788RNP0/ke17ZwdGBToddI8pDm48kDAv91l_EVpAtt3T82Wm1m9Zw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVFYJKH45azV1OdE3sZLTRbs6ZFFp8hOYq1RoRuprWGoaTqWIIaSPh2v08GbKqpiV54/BotonPSE.png" alt=""/>
+                        </div>
                     </div>                    
                 </div>
             </div>
--- src/paginas/Producto.js
+++ src/paginas/Producto.js
@@ -3,6 +3,8 @@ import ListadeProductos from '../components/ListadeProductos'
 import BtnIrATienda from '../components/BtnIrATienda'
 import BtnCarrito from '../components/BtnCarrito'
 import BtnAdministrarProductos from '../components/BtnAdministrarProductos'
+import Barramenu from '../components/estilos/Barramenu.css'
+
 
 
 
@@ -10,10 +12,9 @@ class Producto extends Component {
     state = {
         database: [],
         carrito: {},
-        totalitems:0,
-        totalcompra:0
+        totalitems: 0,
+        totalcompra: 0
     }
-
     async componentDidMount() {
         await this.fetchProductos()
     }
@@ -22,55 +23,51 @@ class Producto extends Component {
         let database = await res.json()
         this.setState({ database })
     }
-
-    handlechange = (e) => {      
-        let {carrito,totalitems,totalcompra}=this.state  
+    handlechange = (e) => {
+        let { carrito, totalitems, totalcompra } = this.state
         let eventval = JSON.parse(e.target.value)
-        let id = eventval.id.toString() 
-        let precio=eventval.precio  
-        
+        let id = eventval.id.toString()
+        let precio = eventval.precio
+
         if (!Object.keys(carrito).includes(id)) {
             console.log('in')
             console.log(id)
             this.setState({
-                carrito:{
+                carrito: {
                     ...carrito,
-                    [id]:1
+                    [id]: 1
                 },
-                totalitems:totalitems+1,
-                totalcompra: totalcompra+Number(precio)
+                totalitems: totalitems + 1,
+                totalcompra: totalcompra + Number(precio)
             })
-            
-        }else{
+
+        } else {
             console.log(carrito[id])
             this.setState({
-                    carrito:{    
+                carrito: {
                     ...carrito,
-                    [id]:carrito[id]+1
+                    [id]: carrito[id] + 1
                 },
-                totalitems:totalitems+1,
-                totalcompra: totalcompra+Number(precio) 
+                totalitems: totalitems + 1,
+                totalcompra: totalcompra + Number(precio)
             })
         }
-        
-        
-      
- 
-        }
+    }
 
     render() {
-        let {carrito,totalitems,database, totalcompra}=this.state
+        let { carrito, totalitems, database, totalcompra } = this.state
         return (
-            
             <div>
-                <BtnIrATienda />
-                <BtnCarrito
-                    database={database}
-                    carrito={carrito}
-                    totalitems={totalitems}
-                    totalcompra={totalcompra}                    
-                />
-                <BtnAdministrarProductos />
+                <div className="barramenu">
+                    <BtnIrATienda />
+                    <BtnCarrito
+                        database={database}
+                        carrito={carrito}
+                        totalitems={totalitems}
+                        totalcompra={totalcompra}
+                    />
+                    <BtnAdministrarProductos />
+                </div>
                 <ListadeProductos
                     productos={database}
                     onChange={this.handlechange}
